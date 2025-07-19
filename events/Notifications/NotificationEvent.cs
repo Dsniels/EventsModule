5b0babb31bbc433bd6955be4c1d115f690629a96
Daniel Salazar <dsnile.salazr@gmail.com>
2025-07-18 22:54:48 -0600

    refactor NotificationEvent class to include sender details and recipient list

--- events/Notifications/NotificationEvent.cs
+++ events/Notifications/NotificationEvent.cs
@@ -1,14 +1,23 @@
 
 namespace events.Notifications;
 
-public class NotificationEvent {
+public class NotificationEvent
+{
 
-  public string Content {get; set;}
-  public DateTime Date {get; set;}
-
-  public NotificationEvent(string content){
-    this.Content = content;
-    this.Date = DateTime.UtcNow;
+  public string SenderName { get; set; }
+  public string SenderId { get; set; }
+  public string SenderImage { get; set; }
+  public string Content { get; set; }
+  public DateTime Date { get; set; }
+  public IList<string> RecipientIds { get; set; }
+  public NotificationEvent(string content, string senderName, string senderId, string senderImage, IList<string> recipientIds)
+  {
+    Content = content;
+    SenderName = senderName;
+    SenderId = senderId;
+    SenderImage = senderImage;
+    Date = DateTime.UtcNow;
+    RecipientIds = recipientIds;
   }
 
 }
